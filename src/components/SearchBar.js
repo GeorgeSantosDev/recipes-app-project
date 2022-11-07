d0bda08886f987fb716d771a22710c9be7d3c576
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-07 19:41:01 -0300

    style: Stylize SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,6 +7,7 @@ import { fetchMealsByIngredient,
 import { fetchDrinksByIngredient,
   fetchDrinksByName,
   fetchDrinksByLetter } from '../services/fetchDrinksFilters';
+import '../styles/SearchBar.css';
 
 function SearchBar() {
   const [filterSelected, setFilterSelected] = useState('');
@@ -59,7 +60,7 @@ function SearchBar() {
 
   const actionFilterMeals = async () => {
     const meals = await mealsFilters[filterSelected](search);
-    console.log(meals);
+
     setRecipesToRender(!meals ? [] : meals);
     actionJustOneRecipeWasReturned(!meals ? [] : meals);
     return !meals ? alertReturn('NotFound') : meals;
@@ -86,54 +87,59 @@ function SearchBar() {
   };
 
   return (
-    <form>
+    <form className="container-flex-collumn form-search-bar">
       <label htmlFor="search">
         <input
           type="text"
           id="search"
           data-testid="search-input"
           placeholder="Search Recipes"
+          className="search-input"
           value={ search }
           onChange={ ({ target: { value } }) => setSearch(value) }
         />
       </label>
-      <label htmlFor="ingredient">
-        Ingredient
-        <input
-          type="radio"
-          id="ingredient"
-          value="ingredient"
-          checked={ filterSelected === 'ingredient' }
-          data-testid="ingredient-search-radio"
-          onChange={ () => { setFilter('ingredient'); } }
-        />
-      </label>
-      <label htmlFor="name">
-        Name
-        <input
-          type="radio"
-          id="name"
-          value="name"
-          checked={ filterSelected === 'name' }
-          data-testid="name-search-radio"
-          onChange={ () => { setFilter('name'); } }
-        />
-      </label>
-      <label htmlFor="first-letter">
-        First Letter
-        <input
-          type="radio"
-          id="first-letter"
-          value="letter"
-          checked={ filterSelected === 'letter' }
-          data-testid="first-letter-search-radio"
-          onChange={ () => { setFilter('letter'); } }
-        />
-      </label>
+
+      <div className="search-radios-inputs-container">
+        <label htmlFor="ingredient">
+          Ingredient
+          <input
+            type="radio"
+            id="ingredient"
+            value="ingredient"
+            checked={ filterSelected === 'ingredient' }
+            data-testid="ingredient-search-radio"
+            onChange={ () => { setFilter('ingredient'); } }
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="radio"
+            id="name"
+            value="name"
+            checked={ filterSelected === 'name' }
+            data-testid="name-search-radio"
+            onChange={ () => { setFilter('name'); } }
+          />
+        </label>
+        <label htmlFor="first-letter">
+          First Letter
+          <input
+            type="radio"
+            id="first-letter"
+            value="letter"
+            checked={ filterSelected === 'letter' }
+            data-testid="first-letter-search-radio"
+            onChange={ () => { setFilter('letter'); } }
+          />
+        </label>
+      </div>
       <button
         type="button"
         data-testid="exec-search-btn"
         onClick={ handleSearch }
+        className="search-btn"
       >
         Search
       </button>
