b0d02f1b5ba0a95df22617e371287867b2a7e84a
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-07 22:18:47 -0300

    style: Stylize RecipeCard component

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../styles/RecipeCard.css';
 
 function RecipeCard({ recipe, index }) {
   const { pathname } = useLocation();
@@ -8,13 +9,14 @@ function RecipeCard({ recipe, index }) {
   const isAtMealsRoute = pathname.includes('meals');
 
   return (
-    <div data-testid={ `${index}-recipe-card` }>
+    <div data-testid={ `${index}-recipe-card` } className="recipe-card-container">
       <img
         src={ isAtMealsRoute ? recipe.strMealThumb : recipe.strDrinkThumb }
         alt={ isAtMealsRoute ? recipe.strMeal : recipe.strDrink }
         data-testid={ `${index}-card-img` }
+        className="recipe-image"
       />
-      <p data-testid={ `${index}-card-name` }>
+      <p data-testid={ `${index}-card-name` } className="recipe-name">
         { isAtMealsRoute ? recipe.strMeal : recipe.strDrink }
       </p>
     </div>
