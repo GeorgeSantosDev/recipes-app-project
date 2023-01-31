e6e7294d4f61ab5ec5b053f108761d8db913216b
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-01-30 21:54:02 -0300

    style: Stylize share btn

--- src/components/ShareBtn.js
+++ src/components/ShareBtn.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import copy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.png';
@@ -6,6 +7,8 @@ import '../styles/ShareBtn.css';
 
 function ShareBtn({ type, id, datatest }) {
   const [isCopy, setIsCopy] = useState(false);
+  const { pathname } = useLocation();
+  console.log(pathname);
 
   const handleClickShareBtn = () => {
     copy(`http://localhost:3000/${type}/${id}`);
@@ -13,18 +16,19 @@ function ShareBtn({ type, id, datatest }) {
   };
 
   return (
-    <div>
-      {
-        isCopy && <p>Link copied!</p>
-      }
+    <div className="share-btn-container">
       <img
         aria-hidden="true"
         data-testid={ datatest }
         src={ shareIcon }
         alt="shareIcon"
         onClick={ handleClickShareBtn }
-        className="share-btn"
+        className={ pathname.includes('done-recipes') ? 'share-btn-dr' : 'share-btn' }
       />
+
+      {
+        isCopy && <span>Link copied!</span>
+      }
     </div>
   );
 }
